7bba645b5444f20a12e329969396692af3ef7687
mpiercy827 <mpiercy@gmail.com>
2018-03-14 22:43:28 -0700

    docs(downstream): Add module docs and method docs

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Downstream is a library for downloading files via HTTPoison response streaming.
 
 - [x] Stream downloads via HTTP GET requests
 - [x] Stream downloads via HTTP POST requests
+- [x] Add module docs and method docs
 
 ## Roadmap
 
 - [ ] Configurable request timeouts
-- [ ] Add support for callbacks (i.e. header handler, status handler, etc.)
-- [ ] Add module docs and method docs
 - [ ] 0.1.0 release on Hex
+- [ ] Add support for callbacks (i.e. header handler, status handler, etc.)
 - [ ] Installation and usage instructions on README
--- lib/downstream.ex
+++ lib/downstream.ex
@@ -1,6 +1,9 @@
 defmodule Downstream do
   @moduledoc """
-  Documentation for Downstream.
+  `Downstream` is an Elixir client for streaming downloads via GET or POST requests,
+  by relying on `HTTPoison`.
+
+  `Downstream` is simple to use, just pass it a URL and an IO device:
   """
 
   alias Downstream.Download
@@ -8,6 +11,12 @@ defmodule Downstream do
   @request_timeout 60_000
 
   @spec get(binary, IO.device(), [{binary, binary}]) :: {atom, binary}
+  @doc ~S"""
+  Downloads from a given URL with a GET request.
+
+  Returns `{:ok, io_device}` if the download is successful, and `{:error, reason}`
+  otherwise.
+  """
   def get(url, io_device, headers \\ []) do
     download_task = Task.async(Download, :stream, [io_device])
 
@@ -16,6 +25,11 @@ defmodule Downstream do
     Task.await(download_task, @request_timeout)
   end
 
+  @doc ~S"""
+  Downloads from the given URL with a GET request, raising an exception in the case of failure.
+
+  If the request succeeds, the IO device is returned.
+  """
   @spec get!(binary, IO.device(), [{binary, binary}]) :: binary
   def get!(url, io_device, headers \\ []) do
     case get(url, io_device, headers) do
@@ -25,6 +39,12 @@ defmodule Downstream do
   end
 
   @spec post(binary, IO.device(), binary | {atom, any}, [{binary, binary}]) :: {atom, binary}
+  @doc ~S"""
+  Downloads from a given URL with a POST request.
+
+  Returns `{:ok, io_device}` if the download is successful, and `{:error, reason}`
+  otherwise.
+  """
   def post(url, io_device, body \\ "", headers \\ []) do
     download_task = Task.async(Download, :stream, [io_device])
 
@@ -34,6 +54,11 @@ defmodule Downstream do
   end
 
   @spec post!(binary, IO.device(), binary | {atom, any}, [{binary, binary}]) :: binary
+  @doc ~S"""
+  Downloads from the given URL with a POST request, raising an exception in the case of failure.
+
+  If the request succeeds, the IO device is returned.
+  """
   def post!(url, io_device, body \\ "", headers \\ []) do
     case post(url, io_device, body, headers) do
       {:error, error} -> raise to_string(error)
